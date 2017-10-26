ba98f2d63404a53aabeb8ee15ea27f97a3e68914
tharindumk <tmadushanka@gmail.com>
2017-10-26 23:44:34 +0530

    Client

---
+++ src/ClientNode.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class ClientNode {
+
+	 public static void main(String args[])
+	    {
+            String myName;
+	        String bootstrapServer = "127.0.0.1";
+	        int port;
+	        InetAddress address;
+	        DatagramSocket socket = null;
+	        DatagramPacket packet;
+	        byte[] sendBuf = new byte[65536];
+
+	        try
+	        {
+	            //sock = new DatagramSocket(55555, )
+
+	            echo("Client Note Started... ..");
+	            String msg = "0036 REG 127.0.0.1 55556 1234abc";
+	                address = InetAddress.getByName("127.0.0.1");
+	                packet = new DatagramPacket(sendBuf, sendBuf.length, 
+	                                                address, 4445);
+	                socket = new DatagramSocket(55555, address);
+	                socket.send(packet);
+	              
+	        }
+
+	        catch(IOException e)
+	        {
+	            System.err.println("IOException " + e);
+	        }
+	    }
+	 
+	  public static void echo(String msg)
+	    {
+	        System.out.println(msg);
+	    }
+}
